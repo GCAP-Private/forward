1dd71b6f0cf1a4ad787ceb642af6c67bb6ae8c18
anguslewis <52469620+anguslewis@users.noreply.github.com>
2023-09-22 14:06:31 -0700

    Update setup.sh to simplify for user

--- setup.sh
+++ setup.sh
@@ -9,39 +9,25 @@ echo
 read -p "Resource identifier (default: sherlock) > "  RESOURCE
 RESOURCE=${RESOURCE:-sherlock}
 
+echo "First, enter your Stanford username."
 echo
 read -p "${RESOURCE} username > "  USERNAME
 
 echo
-echo "Next, pick a port to use.  If someone else is port forwarding using that
-port already, this script will not work.  If you pick a random number in the
+echo "Next, pick a port number to use.  If someone else is port forwarding using 
+that port already, this script will not work.  If you pick a random number in the
 range 49152-65335, you should be good."
 echo
 read -p "Port to use > "  PORT
 
 echo
-echo "Next, pick the ${RESOURCE} partition on which you will be running your
-notebooks.  If your PI has purchased dedicated hardware on ${RESOURCE}, you can use
-that partition.  Otherwise, leave blank to use the default partition (normal)."
-echo
-read -p "${RESOURCE} partition (default: normal) > "  PARTITION
-PARTITION=${PARTITION:-normal}
 
-echo
-SHARE="/scratch/users/vsochat/share"
-echo "A containershare (https://vsoch.github.io/containershare is a library of
-containers that are prebuilt for you, and provided on your cluster resource. if you
-are at Stanford, leave this to be the default. If not, ask your HPC administrator
-about setting one up, and direct them to https://www.github.com/vsoch/containershare."
-echo
-read -p "container shared folder (default for Stanford: ${SHARE}) > " CONTAINERSHARE
-CONTAINERSHARE=${CONTAINERSHARE:-${SHARE}}
-
-echo
-
-MEM=120G
-CORES=3
-TIME=48:00:00
+PARTITION="maggiori,gsb,normal,owners"
+RESOURCE="sherlock"
+MEM="120G"
+CORES="3"
+TIME="48:00:00"
+CONTAINERSHARE="/scratch/users/vsochat/share"
 
 for var in USERNAME PORT PARTITION RESOURCE MEM CORES TIME CONTAINERSHARE
 do
