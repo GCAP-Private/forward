9b8370a18ccf95cc8148fea1a296f1989ddc63cc
anguslewis <52469620+anguslewis@users.noreply.github.com>
2023-09-22 14:08:05 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,15 +3,10 @@
 # Sets up parameters for use with other scripts.  Should be run once.
 # Sample usage: bash setup.sh
 
-echo "First, choose the resource identifier that specifies your cluster resoure. We
-will set up this name in your ssh configuration, and use it to reference the resource (sherlock)."
 echo
-read -p "Resource identifier (default: sherlock) > "  RESOURCE
-RESOURCE=${RESOURCE:-sherlock}
-
 echo "First, enter your Stanford username."
 echo
-read -p "${RESOURCE} username > "  USERNAME
+read -p "Stanford username > "  USERNAME
 
 echo
 echo "Next, pick a port number to use.  If someone else is port forwarding using 
@@ -19,7 +14,6 @@ that port already, this script will not work.  If you pick a random number in th
 range 49152-65335, you should be good."
 echo
 read -p "Port to use > "  PORT
-
 echo
 
 PARTITION="maggiori,gsb,normal,owners"
