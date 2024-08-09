3248b06d107effa1420ae185539925142491b784
Chiara Bargellesi <cbargell@stanford.edu>
2024-08-09 15:07:14 -0700

    Add setup_gpu.sh which generates params_gpu.sh and adapt resume_gpu.sh and start_gpu.sh

--- resume_gpu.sh
+++ resume_gpu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
 # Resumes an already running remote sbatch job.
-# Sample usage: bash resume.sh
+# Sample usage: bash resume_gpu.sh
 
 if [ ! -f params_gpu.sh ]
 then
-    echo "Need to configure params before first run, run setup.sh!"
+    echo "Need to configure params before first run, run setup_gpu.sh!"
     exit
 fi
 source params_gpu.sh
@@ -14,6 +14,6 @@ NAME="${1}"
 
 # The user is required to specify port
 
-echo "ssh ${RESOURCE} squeue --name=$NAME --user=$USERNAME -o "%N" -h"
-MACHINE=`ssh ${RESOURCE} squeue --name=$NAME --user=$USERNAME -o "%N" -h`
+echo "ssh ${RESOURCE} squeue --name=$NAME --user=$RES_USERNAME -o "%N" -h"
+MACHINE=`ssh ${RESOURCE} squeue --name=$NAME --user=$RES_USERNAME -o "%N" -h`
 ssh -L $PORT:localhost:$PORT ${RESOURCE} ssh -L $PORT:localhost:$PORT -N $MACHINE &
